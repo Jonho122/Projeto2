7c077e00d3f0a1ecc64bad024a4282f64ccf6379
John Kevin <jk9200175@gmail.com>
2025-02-11 20:21:55 -0300

    Tarefas

--- app.py/app.py
+++ app.py/app.py
@@ -1,1 +1,19 @@
-import pysimplegui as sg
+import PySimpleGUI as sg
+
+def criar_tarefa():
+ sg.theme('DarkAmber')
+ linha = [
+    [sg.Checkbox(""),sg.Input("")] 
+ ]
+ layout = [
+    [sg.Frame("Tarefas", layout=linha,key='container')],
+    [sg.Button("Adicionar"), sg.Button("Resetar")]
+ ]
+
+ return sg.Window("Todo List", layout=layout, finalize=True)
+#criação da tarefa
+janela = criar_tarefa()
+while True:
+    event, values = janela.read()
+    if event == sg.WIN_CLOSED:
+        break
